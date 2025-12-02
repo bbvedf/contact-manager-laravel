e0a04b0169d27773ed929169a9ad820a61d492be
bbvedf <bbvedf@gmail.com>
2025-12-02 12:46:34 +0100

    Ajustes en modal/eliminación contactos.

--- src/resources/views/layout/partials/scripts.blade.php
+++ src/resources/views/layout/partials/scripts.blade.php
@@ -1,2 +1,3 @@
 <script src="https://ryzenpc.mooo.com/shared/auth-bridge.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 <script src="{{ asset('js/integrated.js') }}"></script>
